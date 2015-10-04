f31df8748fe75655e63f0a742e79fb764d38c0e8
Z <zhqhku@gmail.com>
2015-10-04 17:17:50 +0800

    done derivative of 2 types of neurons

--- net_structure.py
+++ net_structure.py
@@ -9,18 +9,44 @@ import numpy as np
 import node_activity as act
 
 class Net_structure:
+    """
+    glossary:
+    ---------
+             layer:     num layer, excludes the input layer
+                        layer 0, input layer
+                        layer 1, first hidden layer
+                        ...
+                        layer m, output layer
+                 y:     output of a neuron (node), index rule:
+                        y_ij is the jth node on the ith layer
+                 w:     weight between 2 adjacent layers, index rule:
+                        w_abc is the weight between layer a and (a+1),
+                        connecting bth node on layer a and cth node 
+                        on layer (a+1)
+                 b:     bias between 2 adjacent layers, index rule:
+                        b_ij is the bias on the jth node on layer i
+                 c:     cost: e.g., sum[(y - target)^2]
+          c_d_y[i]:     partial derivative of cost w.r.t. ith layer y
+    c_d_w[a][b][c]:     partial derivative of cost w.r.t. ath layer w
+                        connecting bth & cth node on the 2 layers
+       c_d_b[i][j]:     partial derivative of cost w.r.t. ith layer b
+                        operating on the jth node on that layer
+    """
     def __init__(self, layer_size_list, activ_list):
         """
         layer_size_list     let the num of nodes in each layer be Ni --> [N1,N2,...Nm]
                             including the input and output layer
         activ_list          list of node activation object, defining the 
                             activation function as well as derivatives
+                            length of list = num of layers excluding input layer
         """
-        # num_layer is excluding the input layer
+        # -1 to exclude the input layer
         self.num_layer = len(layer_size_list) - 1
+        assert len(activ_list) == self.num_layer
+
         self.w_list = [np.zeros((layer_size_list[i], layer_size_list[i+1]))
-                        for i in xrange(self.num_layer)]
-        self.b_list = [np.zeros(layer_size_list[i+1]) for i in xrange(self.num_layer)]
+                        for i in range(self.num_layer)]
+        self.b_list = [np.zeros(layer_size_list[i+1]) for i in range(self.num_layer)]
         self.activ_list = activ_list
 
     def __str__(self):
@@ -28,7 +54,7 @@ class Net_structure:
         print the value of weight and bias array, for each layer
         """
         net_stat = ''
-        for i in xrange(self.num_layer-1, -1, -1):
+        for i in range(self.num_layer-1, -1, -1):
             net_stat += '--------------------------\n'
             net_stat += 'layer {}: {} nodes\n'.format(i+1, self.w_list[i].shape[1])
             net_stat += '--------------------------\n'
@@ -50,14 +76,16 @@ class Net_structure:
             layer_out   the output from the output layer
         """
         layer_out = data
-        for i in xrange(self.num_layer):
+        for i in range(self.num_layer):
             layer_out = self.activ_list[i] \
-                .act_forward(layer_out, w_list[i], b_list[i])
+                .act_forward(layer_out, self.w_list[i], self.b_list[i])
         return layer_out
 
 
 if __name__ == "__main__":
-    ns = Net_structure([2,3,4], [act.Node_linear] * 3)
+    ns = Net_structure([2,3,4], [act.Node_sigmoid, act.Node_linear])
     print(ns)
-    print act.Node_sigmoid.act_forward(np.array(range(4)).reshape(2,2), np.array(range(6)).reshape(2,3), np.array([3,4,5]))
+    print(ns.act_forward(np.array(range(4)).reshape(2,2)))
+
+    #print act.Node_sigmoid.act_forward(np.array(range(4)).reshape(2,2), np.array(range(6)).reshape(2,3), np.array([3,4,5]))
 
--- node_activity.py
+++ node_activity.py
@@ -9,19 +9,58 @@ class Node_activity:
     __metaclass__ = ABCMeta
     def __init__():
         pass
+    @classmethod
     @abstractmethod
     def act_forward(cls, prev_layer_out, w, b):
         """
-        NOTE: this should be overriden with classmethod
-        """
-        pass
-    @classmethod
-    def pre_process(cls, prev_layer_out, w, b):
-        """
-        do the pre-calculation of the part that is common
-        for almost all type of neurons
+        NOTE: overwrite with classmethod
+
+        do the pre-calculation of feed forward provcess on the 
+        part that is common to almost all types of neurons
+
+        argument:
+        prev_layer_out:     should be 2D, shape[0] is data piece index,
+                            shape[1] is num nodes in lower layer
         """
         return np.dot(prev_layer_out, w) + b
+
+    @classmethod
+    @abstractmethod
+    def y_d_w(cls, y_n, y_n_1, w_idx):
+        """
+        NOTE: overwrite with classmethod
+
+        derivative of y w.r.t. w, where y is the output of 
+        the neuron, and w is the weight connecting y and y 
+        from previous layer.
+        do the pre-calc for the part that is common for all sub-class
+
+        argument:
+        y_n     y list for nth layer
+        y_n_1   y list for (n-1) layer (prev layer)
+        w_idx   index tuple of weight (i.e. w is connecting 
+                which 2 neurons)
+        """
+        # the last dimension of y_n should be num of nodes in nth layer
+        deriv = np.zeros(y_n.shape)
+        deriv[..., w_idx[1]] = y_n_1[..., w_idx[0]]
+        return deriv
+
+    @classmethod
+    @abstractmethod
+    def y_d_b(cls, y_n, b_idx):
+        """
+        NOTE: overwrite with classmethod
+
+        derivative w.r.t. bias
+
+        argument:
+        y_n     y list for nth layer
+        b_idx   bias index, suggesting which node's bias is under concern
+        """
+        deriv = np.zeros(y_n.shape)
+        deriv[..., b_idx] = 1
+        return deriv
 
 
 class Node_linear(Node_activity):
@@ -33,7 +72,14 @@ class Node_linear(Node_activity):
         """
         feed forward linear neuron
         """
-        return super(Node_linear, cls).pre_process(prev_layer_out, w, b)
+        return super(Node_linear, cls).act_forward(prev_layer_out, w, b)
+    @classmethod
+    def y_d_w(cls, y_n, y_n_1, w_idx):
+        return super(Node_linear, cls).y_d_w(y_n, y_n_1, w_idx)
+    
+    @classmethod
+    def y_d_b(cls, y_n, b_idx):
+        return super(Node_linear, cls).y_d_b(y_n, b_idx)
 
 
 class Node_sigmoid(Node_activity):
@@ -45,10 +91,30 @@ class Node_sigmoid(Node_activity):
         """
         feed forward sigmoid neuron
         """
-        z = super(Node_sigmoid, cls).pre_process(prev_layer_out, w, b)
+        z = super(Node_sigmoid, cls).act_forward(prev_layer_out, w, b)
         shape = z.shape
-        size = z.size
+        sz = z.size
         # flatten z and compute exp, then restore original shape
-        expz = np.array(map(lambda x: exp(x), z.reshape(size)))
+        # python3.x: have to convert to list first
+        expz = list(map(lambda x: exp(x), z.reshape(sz)))
+        expz = np.array(expz)
         expz = expz.reshape(shape)
         return 1. / (1 + expz)
+
+    @classmethod
+    def y_d_w(cls, y_n, y_n_1, w_idx):
+        y_n_sub = y_n[..., w_idx[1]]
+        d_sigmo = y_n_sub * (1 - y_n_sub)
+        d_chain = super(Node_sigmoid, cls).y_d_w(y_n, y_n_1, w_idx)[..., w_idx[1]]
+        deriv = np.zeros(y_n.shape)
+        deriv[..., w_idx[1]] = d_sigmo * d_chain
+        return deriv
+
+    @classmethod
+    def y_d_b(cls, y_n, b_idx):
+        y_n_sub = y_n[..., b_idx]
+        d_sigmo = y_n_sub * (1 - y_n_sub)
+        d_chain = super(Node_sigmoid, cls).y_d_b(y_n, b_idx)[..., b_idx]
+        deriv = np.zeros(y_n.shape)
+        deriv[..., b_idx] = d_sigmo * d_chain
+        return deriv
