03deb1986dab778a438cf4434568bfeb007ff21e
zimplex <zhqhku@gmail.com>
2016-01-23 17:55:24 +0800

    change some to instance method

--- conv/conv.py
+++ conv/conv.py
@@ -18,8 +18,10 @@ class Node_conv(Node_activity):
     make sure that the kernel width is equal to kernel height!!
     """
     __metaclass__ = ABCMeta
-    def __init__():
-        pass
+    def __init__(self, stride, padding):
+        self.stride = stride
+        self.padding = padding
+
     @classmethod
     def _get_patch(cls, base_mat, y_start_base, x_start_base, dy, dx, unit):
         """
@@ -94,8 +96,7 @@ class Node_conv(Node_activity):
         
 
 
-    @classmethod
-    def act_forward(cls, prev_layer, w, stride, padding):
+    def act_forward(self, prev_layer, w, b):
         """
         NOTE:
             w is actually the flipped kernel:
@@ -109,8 +110,8 @@ class Node_conv(Node_activity):
             (batch) x (channel_out) x (height') x (width')
             please refer to _conv4dflip for height' and width'
         """
-        ret = cls._conv4dflip(prev_layer, np.swapaxes(w, 0, 1), stride, 1, padding)
-        return np.clip(ret, 0, np.finfo(np.float64).max)    # ReLU
+        ret = self._conv4dflip(prev_layer, np.swapaxes(w, 0, 1), self.stride, 1, self.padding)
+        return np.clip(ret+b, 0, np.finfo(np.float64).max)    # ReLU
 
     @classmethod
     def y_d_x(cls, y_n):
@@ -120,29 +121,28 @@ class Node_conv(Node_activity):
         return np.greater(y_n, 0.)
    
 
-    @classmethod
-    def c_d_w_b_yn1(cls, c_d_yn, y_n, y_n_1, w, stride, padding, is_c_d_yn1=1):
+    def c_d_w_b_yn1(self, c_d_yn, y_n, y_n_1, w, is_c_d_yn1=1):
         """
         stride, padding are both the integer value in the feed forward case.
         get derivative of cost w.r.t. weight, bias, prev_layer.
         """
-        c_d_xn = cls._c_d_xn(c_d_yn, y_n)
+        c_d_xn = self._c_d_xn(c_d_yn, y_n)
         c_d_b = np.sum(c_d_xn, axis=0)
         ####  c_d_w  ####
         ##  y_n_1 (*) flipped(c_d_xn)
         #   patch_stride = stride
         #   padding = padding
         #   slide_stride = 1
-        c_d_w = cls._conv4dflip(np.swapaxes(y_n_1,0,1), 
-                    np.swapaxes(c_d_xn,0,1), 1, stride, padding)
+        c_d_w = self._conv4dflip(np.swapaxes(y_n_1,0,1), 
+                    np.swapaxes(c_d_xn,0,1), 1, self.stride, self.padding)
         assert c_d_w.shape == w.shape
         ####  c_d_yn1  ####
         ##  c_d_xn (*) w ##
         #   patch_stride = 1/stride
         #   padding = (kern-padding-1)/stride
         #   slide_stride = 1/stride
-        pad2 = Fraction(w.shape[-1] - padding - 1, stride)
-        c_d_yn1 = cls._conv4dflip(c_d_xn, w[:,:,::-1,::-1], 
-                    Fraction(1, stride), Fraction(1, stride), pad2)
+        pad2 = Fraction(w.shape[-1] - self.padding - 1, self.stride)
+        c_d_yn1 = self._conv4dflip(c_d_xn, w[:,:,::-1,::-1], 
+                    Fraction(1, self.stride), Fraction(1, self.stride), pad2)
         assert c_d_yn1.shape == y_n_1.shape
         return c_d_w, c_d_b, c_d_yn1
