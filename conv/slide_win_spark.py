c8db783fb0260f0ff85a9a537cc58ad2c64940d4
zimplex <zhqhku@gmail.com>
2016-02-27 17:43:46 +0800

    minor fix

--- conv/slide_win_spark.py
+++ conv/slide_win_spark.py
@@ -100,6 +100,7 @@ def slid_win_4d_flip(base_mat, kern_mat, sliding_stride, patch_stride, padding, 
         for jj in range(s):
                 itr += [np.array((ii,jj))]
     rdd_base_exp = spark.sc.parallelize(itr).map(lambda x: (x,base_mat, spark.rdd_count.add(1)))
+    printf(spark.rdd_count.value)
     t5 = timeit.default_timer()
     RUNTIME['repeat_base_mat'] += t5 - t4
     mn = np.array((int(m), int(n)))
