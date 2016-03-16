68848c896e1bb17a5aff9bdfa077b038ca67c641
zimplex <zhqhku@gmail.com>
2016-03-16 12:58:21 +0800

    factor out cmd

---
+++ ec2/cmd.py
@@ -0,0 +1,124 @@
+CMD = {
+    'get_dns': """
+            name='"{name}"'
+            id=$(aws ec2 describe-instances | grep 'InstanceId' | awk '{{print $2}}')
+            for i in $id
+            do
+                i=$(echo $i | cut -d'"' -f2)
+                k=$(aws ec2 describe-instances --instance-ids $i | grep $name)
+                if [ "$k" != '' ]
+                then
+                    dns=$(aws ec2 describe-instances --instance-ids $i | grep 'PublicDnsName' | awk 'NR==1' | cut -d'"' -f4)
+                    echo $dns
+                    exit
+                fi
+            done
+
+    """,
+    'tar_z': """
+            tar -zcf temp.ignore/temp.tar.gz ../neural-net-bp/ --exclude='.git' --exclude='*ignore*' \
+                --exclude='__pycache__' --exclude='*.db' --exclude='*.pyc' --exclude='*.npz'
+    """,
+    'tar_x': """
+            tar -xzf temp.tar.gz
+    """,
+    'zip': """
+            zip -R packed_module.zip "*.py" #--exclude="*.npz" --exclude="*.git*" --exclude="*.db" --exclude="*.pyc" --exclude="*__pycache__*" --exclude="*ignore*"
+    """,
+    'source_rc': """
+            . /root/{rc}
+    """,
+    'key_id_parse': """
+            credential_f={credential_f}
+            ACCESS_KEY_ID=$(cat $credential_f | awk 'NR==2' | awk -F ',' '{{print $(NF-1)}}')
+            SECRET_ACCESS_KEY=$(cat $credential_f | awk 'NR==2' | awk -F ',' '{{print $NF}}')
+            export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
+            export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
+            echo $ACCESS_KEY_ID
+            echo $SECRET_ACCESS_KEY
+    """,
+    'key_id_export': """
+            export AWS_SECRET_ACCESS_KEY={secret_key}
+            export AWS_ACCESS_KEY_ID={key_id}
+    """,
+    'scp': """
+            scp -rp -i {id} {f} root@{dns}:/root/{to_dir}
+    """,
+    'pipe_remote': """
+            python3 -m ec2.spark_launcher --login --pipe {pipe_args}
+    """,
+    'hdfs_cp': """
+            $hdfs_dir/hadoop distcp s3n://{f} hdfs://
+    """,
+    'hdfs_conf': """
+            hdfs_dir={hdfs}
+            $hdfs_dir/start-all.sh
+            conf_f='/root/ephemeral-hdfs/conf/core-site.xml'
+            line=$(grep -n awsAccessKeyId $conf_f | cut -d: -f1)
+            sed -i "$((line+1))s/.*/<value>{key_id}<\/value>/" $conf_f
+            line=$(grep -n awsSecretAccessKey $conf_f | cut -d: -f1)
+            sed -i "$((line+1))s/.*/<value>{secret}<\/value>/" $conf_f
+    """,
+    'dir_create': """
+            dir={dir}
+            if [ ! -d $dir ]; then mkdir $dir; fi
+    """,
+    'dir_clone': """
+            dir={dir}
+            if [ -d $dir ]; then rm -rf $dir; fi
+            git clone --depth=1 -b ec2_spark {dir_git}
+    """,
+    'dir_clear': """
+            dir=/root/{dir}
+            if [ -d $dir ]; then rm -rf $dir; fi
+    """,
+    'py3_check': """
+            py3_path=$(which python3)
+            if [[ -z $py3_path ]] || [[ $py3_path =~ '/which:' ]]
+            then
+                echo 'python3 is not installed! quit.'
+                exit
+            fi    
+    """,
+    'submit_spark': """
+            master_dns={dns}
+            app_home=/root/{name}/
+            cd $app_home
+            submit_main=$app_home/{main}
+            args="{args}"
+            PYSPARK_PYTHON=$(which python3) \
+            /root/spark/bin/spark-submit --master spark://$master_dns:7077 \
+                --conf spark.eventLog.enabled=true --conf spark.executor.memory=2g --conf spark.driver.memory=2g --py-files packed_module.zip $submit_main $args
+            #/root/spark/bin/spark-submit /root/spark/examples/src/main/python/pi.py 10
+    """,
+    'submit_normal': """
+            app_home=/root/{name}/
+            cd $app_home
+            screen -d -m python3 {main} {args}
+    """,
+    'record_submit_cmd': """
+            echo '{cmd}' > debug_submit.sh
+            chmod 777 debug_submit.sh
+    """,
+    'swap_space': """
+        cur_swap=$(free -m | grep 'Swap' | awk '{print $2}')
+        if ! [[ $cur_swap =~ 40[0-9][0-9] ]]
+        then
+            sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=4096
+            sudo /sbin/mkswap /var/swap.1
+            sudo swapon /var/swap.1
+        fi
+    """,
+    '_': """
+        #aws s3 cp s3://bloodcell/3cat_7500.npz /root/raw_bloodcell.npz
+        cd neural-net-bp
+        python3 -m util.npz_prepare
+        #aws s3 cp /root/raw_bloodcell_part*.npz s3://bloodcell/3cat_7500_scaled.npz
+    """,
+    '__': """
+        cd neural-net-bp
+        python3 -m test.batch_eval /root/maybegood.npz /root/test_batch_dir/
+    """
+}   # _ & __ are temporary commands: for net training on non-spark env
+
+
--- ec2/spark_submit.py
+++ ec2/spark_submit.py
@@ -4,6 +4,9 @@ import re
 import ec2.conf as conf
 import os
 
+
+from ec2.cmd import CMD
+
 import pdb
 
 _OUTPUT_FORMAT = 'json'
@@ -19,136 +22,13 @@ _APP_INFO = {
         'name': 'blood_cell_classification_3cat',
         'submit_main': ['conv_unittest.py','main.py','sweep.py']
 }
-_CMD = {
-    'get_dns': """
-            name='"{name}"'
-            id=$(aws ec2 describe-instances | grep 'InstanceId' | awk '{{print $2}}')
-            for i in $id
-            do
-                i=$(echo $i | cut -d'"' -f2)
-                k=$(aws ec2 describe-instances --instance-ids $i | grep $name)
-                if [ "$k" != '' ]
-                then
-                    dns=$(aws ec2 describe-instances --instance-ids $i | grep 'PublicDnsName' | awk 'NR==1' | cut -d'"' -f4)
-                    echo $dns
-                    exit
-                fi
-            done
-
-    """,
-    'tar_z': """
-            tar -zcf temp.ignore/temp.tar.gz ../neural-net-bp/ --exclude='.git' --exclude='*ignore*' \
-                --exclude='__pycache__' --exclude='*.db' --exclude='*.pyc' --exclude='*.npz'
-    """,
-    'tar_x': """
-            tar -xzf temp.tar.gz
-    """,
-    'zip': """
-            zip -R packed_module.zip "*.py" #--exclude="*.npz" --exclude="*.git*" --exclude="*.db" --exclude="*.pyc" --exclude="*__pycache__*" --exclude="*ignore*"
-    """,
-    'source_rc': """
-            . /root/{rc}
-    """,
-    'key_id_parse': """
-            credential_f={credential_f}
-            ACCESS_KEY_ID=$(cat $credential_f | awk 'NR==2' | awk -F ',' '{{print $(NF-1)}}')
-            SECRET_ACCESS_KEY=$(cat $credential_f | awk 'NR==2' | awk -F ',' '{{print $NF}}')
-            export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
-            export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
-            echo $ACCESS_KEY_ID
-            echo $SECRET_ACCESS_KEY
-    """,
-    'key_id_export': """
-            export AWS_SECRET_ACCESS_KEY={secret_key}
-            export AWS_ACCESS_KEY_ID={key_id}
-    """,
-    'scp': """
-            scp -rp -i {id} {f} root@{dns}:/root/{to_dir}
-    """,
-    'pipe_remote': """
-            python3 -m ec2.spark_launcher --login --pipe {pipe_args}
-    """,
-    'hdfs_cp': """
-            $hdfs_dir/hadoop distcp s3n://{f} hdfs://
-    """,
-    'hdfs_conf': """
-            hdfs_dir={hdfs}
-            $hdfs_dir/start-all.sh
-            conf_f='/root/ephemeral-hdfs/conf/core-site.xml'
-            line=$(grep -n awsAccessKeyId $conf_f | cut -d: -f1)
-            sed -i "$((line+1))s/.*/<value>{key_id}<\/value>/" $conf_f
-            line=$(grep -n awsSecretAccessKey $conf_f | cut -d: -f1)
-            sed -i "$((line+1))s/.*/<value>{secret}<\/value>/" $conf_f
-    """,
-    'dir_create': """
-            dir={dir}
-            if [ ! -d $dir ]; then mkdir $dir; fi
-    """,
-    'dir_clone': """
-            dir={dir}
-            if [ -d $dir ]; then rm -rf $dir; fi
-            git clone --depth=1 -b ec2_spark {dir_git}
-    """,
-    'dir_clear': """
-            dir=/root/{dir}
-            if [ -d $dir ]; then rm -rf $dir; fi
-    """,
-    'py3_check': """
-            py3_path=$(which python3)
-            if [[ -z $py3_path ]] || [[ $py3_path =~ '/which:' ]]
-            then
-                echo 'python3 is not installed! quit.'
-                exit
-            fi    
-    """,
-    'submit_spark': """
-            master_dns={dns}
-            app_home=/root/{name}/
-            cd $app_home
-            submit_main=$app_home/{main}
-            args="{args}"
-            PYSPARK_PYTHON=$(which python3) \
-            /root/spark/bin/spark-submit --master spark://$master_dns:7077 \
-                --conf spark.eventLog.enabled=true --conf spark.executor.memory=2g --conf spark.driver.memory=2g --py-files packed_module.zip $submit_main $args
-            #/root/spark/bin/spark-submit /root/spark/examples/src/main/python/pi.py 10
-    """,
-    'submit_normal': """
-            app_home=/root/{name}/
-            cd $app_home
-            screen -d -m python3 {main} {args}
-    """,
-    'record_submit_cmd': """
-            echo '{cmd}' > debug_submit.sh
-            chmod 777 debug_submit.sh
-    """,
-    'swap_space': """
-        cur_swap=$(free -m | grep 'Swap' | awk '{print $2}')
-        if ! [[ $cur_swap =~ 40[0-9][0-9] ]]
-        then
-            sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=4096
-            sudo /sbin/mkswap /var/swap.1
-            sudo swapon /var/swap.1
-        fi
-    """,
-    '_': """
-        #aws s3 cp s3://bloodcell/3cat_7500.npz /root/raw_bloodcell.npz
-        cd neural-net-bp
-        python3 -m util.npz_prepare
-        #aws s3 cp /root/raw_bloodcell_part*.npz s3://bloodcell/3cat_7500_scaled.npz
-    """,
-    '__': """
-        cd neural-net-bp
-        python3 -m test.batch_eval /root/maybegood.npz /root/test_batch_dir/
-    """
-}   # _ & __ are temporary commands: for net training on non-spark env
-
 
 def conf_AWS_CLI(credential_f, region):
     """
     setup default values for aws command line interface.
     """
     try:
-        scriptGetKey = _CMD['key_id_parse'].format(credential_f=credential_f)
+        scriptGetKey = CMD['key_id_parse'].format(credential_f=credential_f)
         stdout, stderr = runScript(scriptGetKey, output_opt='pipe')
         key_id, secret_key= stdout.decode('utf-8').split('\n')[:-1]
         os.environ['AWS_SECRET_ACCESS_KEY'] = secret_key
@@ -168,7 +48,7 @@ def get_DNS(name):
     get the ID and public-DNS of node
     """
     try:
-        stdout, stderr = runScript(_CMD['get_dns'].format(name=name), output_opt='pipe')
+        stdout, stderr = runScript(CMD['get_dns'].format(name=name), output_opt='pipe')
         printf('get public dns for {}', name)
         return stdout.decode('utf-8').split('\n')[0]
     except ScriptException as se:
@@ -209,32 +89,32 @@ def prepare(id_f, master_dns, credential_f, key_id, secret_key, is_hdfs=True, is
         if is_scp:
             # ____
             for f in [credential_f, 'ec2/'+_CUS_BASHRC]:#, 'train_data/usps.npz', 'train_data/3cat_1000_scale.npz']:
-                scpScript = _CMD['scp'].format(id=id_f, f=f, dns=master_dns, to_dir='')
+                scpScript = CMD['scp'].format(id=id_f, f=f, dns=master_dns, to_dir='')
                 stdout, stderr = runScript(scpScript, output_opt='display', input_opt='display')
                 printf(scpScript, type='WARN')
         if is_clone:
-            cmd  = [_CMD['zip']]
-            cmd += [_CMD['tar_z']]
-            cmd += [_CMD['scp'].format(id=id_f, f='temp.ignore/temp.tar.gz', dns=master_dns, to_dir='')]
+            cmd  = [CMD['zip']]
+            cmd += [CMD['tar_z']]
+            cmd += [CMD['scp'].format(id=id_f, f='temp.ignore/temp.tar.gz', dns=master_dns, to_dir='')]
             cmd  = '\n'.join(cmd)
             stdout, stderr = runScript(cmd, output_opt='display', input_opt='display')
 
         app_root = _APP_INFO['repo_url'].split('/')[-1].split('.git')[0]
         combineCmd  = []
-        combineCmd += [_CMD['source_rc'].format(rc=_CUS_BASHRC)]
-        combineCmd += [_CMD['key_id_export'].format(key_id=key_id, secret_key=secret_key)]
+        combineCmd += [CMD['source_rc'].format(rc=_CUS_BASHRC)]
+        combineCmd += [CMD['key_id_export'].format(key_id=key_id, secret_key=secret_key)]
         if is_hdfs:
-            combineCmd += [_CMD['hdfs_conf']\
+            combineCmd += [CMD['hdfs_conf']\
                 .format(hdfs=_AWS_DIR_INFO['hdfs'], key_id=key_id, secret=secret_key)]
-            combineCmd += [_CMD['hdfs_cp'].format(f=_AWS_DIR_INFO['data'])]
-        combineCmd += [_CMD['dir_create'].format(dir='/tmp/spark-events/')]
+            combineCmd += [CMD['hdfs_cp'].format(f=_AWS_DIR_INFO['data'])]
+        combineCmd += [CMD['dir_create'].format(dir='/tmp/spark-events/')]
         if is_clone:
-            combineCmd += [_CMD['tar_x']]
-        combineCmd += [_CMD['py3_check']]
-        combineCmd += [_CMD['swap_space']]
+            combineCmd += [CMD['tar_x']]
+        combineCmd += [CMD['py3_check']]
+        combineCmd += [CMD['swap_space']]
         combineCmd += ['exit\n']
         combineCmd = '\n'.join(combineCmd)
-        remoteScript = _CMD['pipe_remote'].format(pipe_args=pipe_args)
+        remoteScript = CMD['pipe_remote'].format(pipe_args=pipe_args)
         printf(remoteScript)
         
         stdout, stderr = runScript(remoteScript, output_opt='display', input_opt='pipe', input_pipe=[combineCmd, '.quit'])
@@ -247,17 +127,17 @@ def prepare(id_f, master_dns, credential_f, key_id, secret_key, is_hdfs=True, is
 def submit_application(name, master_dns, main, args_main, key_id='', secret_key='', pipe_args=''):
     printf('ENTER application submission', type='WARN')
     try:
-        shot = [_CMD['source_rc'].format(rc=_CUS_BASHRC)]
+        shot = [CMD['source_rc'].format(rc=_CUS_BASHRC)]
         if main in ['conv_unittest.py']:
-            submit_cmd = _CMD['submit_spark'].format(dns=master_dns, name=name, main=main, args=args_main)
+            submit_cmd = CMD['submit_spark'].format(dns=master_dns, name=name, main=main, args=args_main)
         elif main in ['sweep.py', 'main.py']:
-            submit_cmd = _CMD['submit_normal'].format(name=name, main=main, args=args_main)
-        shot += [_CMD['key_id_export'].format(key_id=key_id,secret_key=secret_key)]
-        shot += [_CMD['record_submit_cmd'].format(cmd=';'.join(submit_cmd.split('\n')[1:]))]
+            submit_cmd = CMD['submit_normal'].format(name=name, main=main, args=args_main)
+        shot += [CMD['key_id_export'].format(key_id=key_id,secret_key=secret_key)]
+        shot += [CMD['record_submit_cmd'].format(cmd=';'.join(submit_cmd.split('\n')[1:]))]
         shot += [submit_cmd]
         shot += ['exit\n']
         shot = '\n'.join(shot)
-        remoteScript = _CMD['pipe_remote'].format(pipe_args=pipe_args)
+        remoteScript = CMD['pipe_remote'].format(pipe_args=pipe_args)
         stdout, stderr = runScript(remoteScript, output_opt='display', input_opt='pipe', input_pipe=[shot, '.quit'])
         printf('submit to spark: {}', shot, type='WARN')
     except ScriptException as se:
