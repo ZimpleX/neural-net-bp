minor fix for normalizing and expanding target

util/npz_prepare.py
@@ -50,8 +50,11 @@ def normalize_exp_col(path_npz, num_op=None, scale_axis=(1,2,3)):
             raw_tag = raw[tag].flatten().astype(np.int)
             if num_op is None:
                 num_op = raw_tag.max() - raw_tag.min() + 1
+                tag_min = raw_tag.min()
+            else:
+                tag_min = 0
             z = np.zeros((num_entry, num_op))
-            z[np.arange(num_entry), raw_tag] = 1.
+            z[np.arange(num_entry), raw_tag-tag_min] = 1.
             ret[tag] = z
 
     np.savez(path_npz, **ret)
@@ -59,16 +62,24 @@ def normalize_exp_col(path_npz, num_op=None, scale_axis=(1,2,3)):
 
 
 def normalize_raw_data(raw, path_out, start_idx, end_idx, num_op=None, scale_axis=(1,2,3)):
+    """
+    raw is loaded npz data;
+    path_out is str
+    """
     data = raw['data'][start_idx:end_idx]
-    target = raw['target'][start_idx:end_idx]
+    target = raw['target'][start_idx:end_idx].astype(int)
     ret = {}
     # prepare target
-    if len(target.shape) == 1:
+    if (len(target.shape) == 1) or \
+       (len(target.shape) == 2 and 1 in target.shape):
         num_entry = target.shape[0]
         if num_op is None:
             num_op = target.max() - target.min() + 1
+            target_min = target.min()
+        else:
+            target_min = 0
         _ = np.zeros((num_entry, num_op))
-        _[np.arange(num_entry), target] = 1.
+        _[np.arange(num_entry), target-target_min] = 1.
         ret['target'] = _
     # prepare data
     rmin = np.min(data, axis=scale_axis)
@@ -102,9 +113,19 @@ if __name__ == '__main__':
     #input_data = 'train_data/3cat_7500.npz'
     #prepare_small_npz(input_data, output_data, 750, 250, 0)
     #normalize_exp_col(output_data, num_op=3)
-    path_in = 'train_data/3cat_7500.npz'
-    raw = np.load(path_in)
-    for s in range(0,7500,1500):
-        printf("idx starting: {}", s)
-        path_out = 'train_data/raw_bloodcell_part_{}.npz'.format(s)
-        normalize_raw_data(raw, path_out, s, s+1500)
+
+    #path_in = 'train_data/3cat_7500.npz'
+    #raw = np.load(path_in)
+    #for s in range(0,7500,1500):
+    #    printf("idx starting: {}", s)
+    #    path_out = 'train_data/raw_bloodcell_part_{}.npz'.format(s)
+    #    normalize_raw_data(raw, path_out, s, s+1500)
+
+    import os
+    _dir = '../../Temp/temp_norm_npz/'
+    f_list = os.listdir(_dir)
+    f_list = ['{}/{}'.format(_dir,i) for i in f_list]
+    for f in f_list:
+        raw = np.load(f)
+        normalize_raw_data(raw,f,0,10000, num_op=9)
+        printf('done normalizing and expanding {}', f)